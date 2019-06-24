a36713fcaa86b6ef7fef223e5ed8aa2cb873461d
g.colo@logicsystems.fr <Gerard>
2019-06-24 09:27:08 +0200

    ProjetA initialisation

---
+++ ProjetA/UniteA/UniteA.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef UniteAH
+#define UniteAH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TFormUniteA : public TForm
+{
+__published:	// Composants gérés par l'EDI
+private:	// Déclarations utilisateur
+public:		// Déclarations utilisateur
+	__fastcall TFormUniteA(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormUniteA *FormUniteA;
+//---------------------------------------------------------------------------
+#endif
